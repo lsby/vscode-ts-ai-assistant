b9f2e67fbf95a249b862b8d0a176d6aa8ef9a57b
hbybyyang <2451759073@qq.com>
2024-08-27 05:16:20 +0800

    用函数组合方式重写ast

---
+++ src/model/ast/program.ts
@@ -0,0 +1,29 @@
+import path from 'path'
+import ts from 'typescript'
+import { 获得源文件们 } from './source-file'
+
+export function 创建程序(tsconfig路径: string): ts.Program {
+  const tsconfig内容 = ts.readConfigFile(tsconfig路径, ts.sys.readFile)
+  const tsconfig解析结果 = ts.parseJsonConfigFileContent(tsconfig内容.config, ts.sys, path.dirname(tsconfig路径))
+  return ts.createProgram(tsconfig解析结果.fileNames, tsconfig解析结果.options)
+}
+export function 获得类型检查器(a: ts.Program): ts.TypeChecker {
+  return a.getTypeChecker()
+}
+/**
+ * 可以使用 {@link 获得源文件们}
+ * 注意将传入路径和源文件路径都做标准化处理, 使用path模块
+ */
+export function 按路径选择源文件(目标路径: string, a: ts.Program): ts.SourceFile | null {
+  const 源文件们 = 获得源文件们(a)
+  const 标准化目标路径 = path.normalize(目标路径)
+
+  for (const 源文件 of 源文件们) {
+    const 标准化源文件路径 = path.normalize(源文件.fileName)
+    if (标准化源文件路径 === 标准化目标路径) {
+      return 源文件
+    }
+  }
+
+  return null
+}
---
+++ src/model/ast/types/types.ts
@@ -0,0 +1,4 @@
+import ts from 'typescript'
+
+export type 类型节点 = ts.TypeAliasDeclaration | ts.InterfaceDeclaration
+export type 函数节点 = ts.FunctionDeclaration
